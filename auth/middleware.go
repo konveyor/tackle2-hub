mansam/tackle-hub

Implement Keycloak-based auth flow

auth/middleware.go
@@ -1,0 +1,37 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/konveyor/tackle2-hub/settings"
+	"net/http"
+	"strings"
+)
+
+//
+// AuthorizationRequired enforces that the user (identified by a token) has
+// been granted the necessary scope to access a resource.
+func AuthorizationRequired(p Provider, requiredScope string) func(*gin.Context) {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		secret := settings.Settings.Auth.AddonAccessSecret
+		if secret != "" && token == secret {
+			c.Next()
+			return
+		}
+
+		scopes, err := p.Scopes(token)
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		for _, s := range scopes {
+			if s.Check(requiredScope, strings.ToLower(c.Request.Method)) {
+				c.Next()
+				return
+			}
+		}
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+}

auth/provider.go
@@ -1,0 +1,129 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"github.com/Nerzal/gocloak/v10"
+	"github.com/golang-jwt/jwt/v4"
+	"strings"
+	"time"
+)
+
+type Provider interface {
+	// Scopes decodes a list of scopes from the token.
+	Scopes(token string) ([]Scope, error)
+}
+
+//
+// Scope represents an authorization scope.
+type Scope interface {
+	// Check whether the scope gives access to the resource with the method.
+	Check(resource string, method string) bool
+}
+
+//
+// NoAuth provider always permits access.
+type NoAuth struct{}
+
+//
+// Scopes decodes a list of scopes from the token.
+// For the NoAuth provider, this just returns a single instance
+// of the NoAuthScope.
+func (r *NoAuth) Scopes(token string) (scopes []Scope, err error) {
+	scopes = append(scopes, &NoAuthScope{})
+	return
+}
+
+//
+// NoAuthScope always permits access.
+type NoAuthScope struct{}
+
+//
+// Check whether the scope gives access to the resource with the method.
+func (r *NoAuthScope) Check(_ string, _ string) (ok bool) {
+	ok = true
+	return
+}
+
+//
+// NewKeycloak builds a new Keycloak auth provider.
+func NewKeycloak(host, realm, id, secret string) (k Keycloak) {
+	client := gocloak.NewClient(host)
+	k = Keycloak{
+		host:   host,
+		realm:  realm,
+		id:     id,
+		secret: secret,
+		client: client,
+	}
+	return
+}
+
+//
+// Keycloak auth provider
+type Keycloak struct {
+	client gocloak.GoCloak
+	host   string
+	realm  string
+	id     string
+	secret string
+}
+
+//
+// Scopes decodes a list of scopes from the token.
+func (r *Keycloak) Scopes(token string) (scopes []Scope, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+	decoded, _, err := r.client.DecodeAccessToken(ctx, token, r.realm)
+	if err != nil {
+		err = errors.New("invalid token")
+		return
+	}
+	if !decoded.Valid {
+		err = errors.New("invalid token")
+		return
+	}
+	claims, ok := decoded.Claims.(*jwt.MapClaims)
+	if !ok || claims == nil {
+		err = errors.New("invalid token")
+		return
+	}
+	rawClaimScopes, ok := (*claims)["scope"].(string)
+	if !ok {
+		err = errors.New("invalid token")
+		return
+	}
+	claimScopes := strings.Split(rawClaimScopes, " ")
+	for _, s := range claimScopes {
+		scope := NewKeycloakScope(s)
+		scopes = append(scopes, &scope)
+	}
+	return
+}
+
+//
+// NewKeycloakScope builds a Scope object from a string.
+func NewKeycloakScope(s string) (scope KeycloakScope) {
+	if strings.Contains(s, ":") {
+		segments := strings.Split(s, ":")
+		scope.resource = segments[0]
+		scope.method = segments[1]
+	} else {
+		scope.resource = s
+	}
+	return
+}
+
+//
+// KeycloakScope is a scope decoded from a Keycloak token.
+type KeycloakScope struct {
+	resource string
+	method   string
+}
+
+//
+// Check whether the scope gives access to the resource with the method.
+func (r *KeycloakScope) Check(resource string, method string) (ok bool) {
+	ok = r.resource == resource && r.method == method
+	return
+}

settings/auth.go
@@ -1,0 +1,54 @@
+package settings
+
+import (
+	"os"
+)
+
+const (
+	EnvAuthRequired         = "AUTH_REQUIRED"
+	EnvKeycloakHost         = "KEYCLOAK_HOST"
+	EnvKeycloakRealm        = "KEYCLOAK_REALM"
+	EnvKeycloakClientID     = "KEYCLOAK_CLIENT_ID"
+	EnvKeycloakClientSecret = "KEYCLOAK_CLIENT_SECRET"
+	EnvAddonAccessSecret    = "ADDON_ACCESS_SECRET"
+)
+
+type Auth struct {
+	// Auth required
+	Required bool
+	// Keycloak client config
+	Keycloak struct {
+		Host         string
+		Realm        string
+		ClientID     string
+		ClientSecret string
+	}
+	// Addon API access secret
+	AddonAccessSecret string
+}
+
+func (r *Auth) Load() (err error) {
+	var found bool
+	r.Required = getEnvBool(EnvAuthRequired, true)
+	r.Keycloak.Host, found = os.LookupEnv(EnvKeycloakHost)
+	if r.Required && !found {
+		panic("KEYCLOAK_HOST required")
+	}
+	r.Keycloak.Realm, found = os.LookupEnv(EnvKeycloakRealm)
+	if r.Required && !found {
+		panic("KEYCLOAK_REALM required")
+	}
+	r.Keycloak.ClientID, found = os.LookupEnv(EnvKeycloakClientID)
+	if r.Required && !found {
+		panic("KEYCLOAK_CLIENT_ID required")
+	}
+	r.Keycloak.ClientSecret, found = os.LookupEnv(EnvKeycloakClientSecret)
+	if r.Required && !found {
+		panic("KEYCLOAK_CLIENT_SECRET required")
+	}
+	r.AddonAccessSecret, found = os.LookupEnv(EnvAddonAccessSecret)
+	if r.Required && !found {
+		panic("API_ACCESS_SECRET required")
+	}
+	return
+}

konveyor/tackle2-hub

auth/middleware.go
@@ -1,0 +1,37 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/konveyor/tackle2-hub/settings"
+	"net/http"
+	"strings"
+)
+
+//
+// AuthorizationRequired enforces that the user (identified by a token) has
+// been granted the necessary scope to access a resource.
+func AuthorizationRequired(p Provider, requiredScope string) func(*gin.Context) {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		secret := settings.Settings.Auth.AddonAccessSecret
+		if secret != "" && token == secret {
+			c.Next()
+			return
+		}
+
+		scopes, err := p.Scopes(token)
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		for _, s := range scopes {
+			if s.Allow(requiredScope, strings.ToLower(c.Request.Method)) {
+				c.Next()
+				return
+			}
+		}
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+}

auth/provider.go
@@ -1,0 +1,129 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"github.com/Nerzal/gocloak/v10"
+	"github.com/golang-jwt/jwt/v4"
+	"strings"
+	"time"
+)
+
+type Provider interface {
+	// Scopes decodes a list of scopes from the token.
+	Scopes(token string) ([]Scope, error)
+}
+
+//
+// Scope represents an authorization scope.
+type Scope interface {
+	// Allow determines whether the scope gives access to the resource with the method.
+	Allow(resource string, method string) bool
+}
+
+//
+// NoAuth provider always permits access.
+type NoAuth struct{}
+
+//
+// Scopes decodes a list of scopes from the token.
+// For the NoAuth provider, this just returns a single instance
+// of the NoAuthScope.
+func (r *NoAuth) Scopes(token string) (scopes []Scope, err error) {
+	scopes = append(scopes, &NoAuthScope{})
+	return
+}
+
+//
+// NoAuthScope always permits access.
+type NoAuthScope struct{}
+
+//
+// Check whether the scope gives access to the resource with the method.
+func (r *NoAuthScope) Allow(_ string, _ string) (ok bool) {
+	ok = true
+	return
+}
+
+//
+// NewKeycloak builds a new Keycloak auth provider.
+func NewKeycloak(host, realm, id, secret string) (k Keycloak) {
+	client := gocloak.NewClient(host)
+	k = Keycloak{
+		host:   host,
+		realm:  realm,
+		id:     id,
+		secret: secret,
+		client: client,
+	}
+	return
+}
+
+//
+// Keycloak auth provider
+type Keycloak struct {
+	client gocloak.GoCloak
+	host   string
+	realm  string
+	id     string
+	secret string
+}
+
+//
+// Scopes decodes a list of scopes from the token.
+func (r *Keycloak) Scopes(token string) (scopes []Scope, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+	decoded, _, err := r.client.DecodeAccessToken(ctx, token, r.realm)
+	if err != nil {
+		err = errors.New("invalid token")
+		return
+	}
+	if !decoded.Valid {
+		err = errors.New("invalid token")
+		return
+	}
+	claims, ok := decoded.Claims.(*jwt.MapClaims)
+	if !ok || claims == nil {
+		err = errors.New("invalid token")
+		return
+	}
+	rawClaimScopes, ok := (*claims)["scope"].(string)
+	if !ok {
+		err = errors.New("invalid token")
+		return
+	}
+	claimScopes := strings.Split(rawClaimScopes, " ")
+	for _, s := range claimScopes {
+		scope := NewKeycloakScope(s)
+		scopes = append(scopes, &scope)
+	}
+	return
+}
+
+//
+// NewKeycloakScope builds a Scope object from a string.
+func NewKeycloakScope(s string) (scope KeycloakScope) {
+	if strings.Contains(s, ":") {
+		segments := strings.Split(s, ":")
+		scope.resource = segments[0]
+		scope.method = segments[1]
+	} else {
+		scope.resource = s
+	}
+	return
+}
+
+//
+// KeycloakScope is a scope decoded from a Keycloak token.
+type KeycloakScope struct {
+	resource string
+	method   string
+}
+
+//
+// Allow determines whether the scope gives access to the resource with the method.
+func (r *KeycloakScope) Allow(resource string, method string) (ok bool) {
+	ok = r.resource == resource && r.method == method
+	return
+}

settings/auth.go
@@ -1,0 +1,54 @@
+package settings
+
+import (
+	"os"
+)
+
+const (
+	EnvAuthRequired         = "AUTH_REQUIRED"
+	EnvKeycloakHost         = "KEYCLOAK_HOST"
+	EnvKeycloakRealm        = "KEYCLOAK_REALM"
+	EnvKeycloakClientID     = "KEYCLOAK_CLIENT_ID"
+	EnvKeycloakClientSecret = "KEYCLOAK_CLIENT_SECRET"
+	EnvAddonAccessSecret    = "ADDON_ACCESS_SECRET"
+)
+
+type Auth struct {
+	// Auth required
+	Required bool
+	// Keycloak client config
+	Keycloak struct {
+		Host         string
+		Realm        string
+		ClientID     string
+		ClientSecret string
+	}
+	// Addon API access secret
+	AddonAccessSecret string
+}
+
+func (r *Auth) Load() (err error) {
+	var found bool
+	r.Required = getEnvBool(EnvAuthRequired, true)
+	r.Keycloak.Host, found = os.LookupEnv(EnvKeycloakHost)
+	if r.Required && !found {
+		panic("KEYCLOAK_HOST required")
+	}
+	r.Keycloak.Realm, found = os.LookupEnv(EnvKeycloakRealm)
+	if r.Required && !found {
+		panic("KEYCLOAK_REALM required")
+	}
+	r.Keycloak.ClientID, found = os.LookupEnv(EnvKeycloakClientID)
+	if r.Required && !found {
+		panic("KEYCLOAK_CLIENT_ID required")
+	}
+	r.Keycloak.ClientSecret, found = os.LookupEnv(EnvKeycloakClientSecret)
+	if r.Required && !found {
+		panic("KEYCLOAK_CLIENT_SECRET required")
+	}
+	r.AddonAccessSecret, found = os.LookupEnv(EnvAddonAccessSecret)
+	if r.Required && !found {
+		panic("ADDON_ACCESS_SECRET required")
+	}
+	return
+}