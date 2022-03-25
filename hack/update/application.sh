08dfef9777f079c063c252ab5e2791087ac88fda
Jeff Ortel <jortel@redhat.com>
2022-03-25 10:00:32 -0700

    Fix PUT on application. fixes #42

--- hack/update/application.sh
+++ hack/update/application.sh
@@ -4,8 +4,8 @@ host="${HOST:-localhost:8080}"
 
 curl -X PUT ${host}/applications/1 -d \
 '{
-    "name":"Cat",
-    "description": "Cat application.",
+    "name":"Dog2",
+    "description": "Dog2 application.",
     "businessService": {"id":1},
     "identities": [
       {"id":1},
