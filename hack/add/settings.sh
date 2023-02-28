9435bbe2f43e4ed23f69e2547c541a1d5e5de2ac
Samuel Lucidi <sam@samlucidi.com>
2023-02-28 15:40:47 -0500

    :sparkles: Allow creating settings by key (#249)
    Fixes https://github.com/konveyor/tackle2-hub/issues/234
    
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- hack/add/settings.sh
+++ hack/add/settings.sh
@@ -4,4 +4,4 @@ host="${HOST:-localhost:8080}"
 
 curl -X POST ${host}/settings -d '{"key":"test.boolean","value":false}' | jq .
 curl -X POST ${host}/settings -d '{"key":"test.numbers","value":[1,2,3]}' | jq .
-
+curl -X POST ${host}/settings/test.bykey -d '123'
