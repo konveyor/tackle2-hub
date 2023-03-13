8624f9db7f5bb7649cd19473f3bf949eb2f34180
Marek Aufart <maufart@redhat.com>
2023-03-13 15:59:55 +0100

    Sample tests init
    Signed-off-by: Marek Aufart <maufart@redhat.com>

---
+++ api/application_test.go
@@ -0,0 +1,62 @@
+package api
+
+// or api_test for blackbox testing
+
+import (
+	"testing"
+)
+
+func TestApplicationsCRUD(t *testing.T) {
+	//tests := []util.Test{
+	//	{
+	//		Name: "Create sample Pathfinder application",
+	//
+	//		Application: Application{
+	//			Name:        "Pathfinder",
+	//			Description: "Tackle Pathfinder application.",
+	//			Repository: &Repository{
+	//				Kind:   "git",
+	//				URL:    "https://github.com/konveyor/tackle-pathfinder.git",
+	//				Branch: "1.2.0",
+	//			},
+	//		},
+	//		ShouldError: false,
+	//	},
+	//	{
+	//		Name: "Create minimalist application",
+	//		Application: api.Application{
+	//			Name: "App1",
+	//		},
+	//		ShouldError: false,
+	//	},
+	//}
+
+	// Setup Hub API client
+	//	hub, err := util.NewHubClient()
+	//	if err != nil {
+	//		t.Fatalf("Unable connect to Hub API: %v", err.Error())
+	//	}
+	//
+	//	// Execute test steps
+	//	for _, tc := range tests {
+	//		t.Log(tc.Name)
+	//
+	//		// Create the application
+	//		err = hub.Post(ApplicationsRoot, &tc.Application)
+	//		if err != nil && !tc.ShouldError {
+	//			t.Errorf("Unexpected application create error: %v", err.Error())
+	//		}
+	//		if err == nil && tc.ShouldError {
+	//			t.Errorf("Expected application create error and didn't get it")
+	//		}
+	//		t.Log(tc.Application)
+	//
+	//		// Clean the application
+	//		if err == nil && !tc.ShouldError {
+	//			err = hub.Delete(fmt.Sprintf("%s/%d", ApplicationsRoot, tc.Application.ID))
+	//			if err != nil && !tc.ShouldError {
+	//				t.Errorf("Unexpected application delete error: %v", err.Error())
+	//			}
+	//		}
+	//	}
+}
---
+++ test/README.md
@@ -0,0 +1,5 @@
+# Hub tests
+
+## REST API
+
+Blackbox tests
