67bc4b91fbd4c6b5bf8d5530af5360e1a94b3596
Marek Aufart <maufart@redhat.com>
2023-04-14 16:16:17 +0200

    Un-alias client pkg in integration test
    Signed-off-by: Marek Aufart <maufart@redhat.com>

--- test/integration/applications-inventory/analysis/pkg.go
+++ test/integration/applications-inventory/analysis/pkg.go
@@ -4,12 +4,12 @@ import (
 	"time"
 
 	"github.com/konveyor/tackle2-hub/api"
-	c "github.com/konveyor/tackle2-hub/test/api/client"
+	"github.com/konveyor/tackle2-hub/test/api/client"
 )
 
 var (
 	// Setup Hub API client
-	Client = c.Client
+	Client = client.Client
 
 	// Analysis waiting loop 5 minutes (60 * 5s)
 	Retry = 60
