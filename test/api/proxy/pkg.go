3bd44e4e2b090a882b219888b3641c53a17c47c2
Marek Aufart <aufi.cz@gmail.com>
2023-05-17 08:51:12 +0200

    :seedling: Add Identity and Proxy to API tests. (#331)

---
+++ test/api/proxy/pkg.go
@@ -0,0 +1,20 @@
+package proxy
+
+import (
+	"github.com/konveyor/tackle2-hub/binding"
+	"github.com/konveyor/tackle2-hub/test/api/client"
+)
+
+var (
+	RichClient *binding.RichClient
+	Proxy binding.Proxy
+)
+
+
+func init() {
+	// Prepare RichClient and login to Hub API (configured from env variables).
+	RichClient = client.PrepareRichClient()
+
+	// Shortcut for Proxy-related RichClient methods.
+	Proxy = RichClient.Proxy
+}
