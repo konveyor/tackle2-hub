50d8ef7296e48e2acc7c521b611b61d525f9b32d
Jeff Ortel <jortel@redhat.com>
2023-05-19 13:32:59 -0500

    :ghost: Add code snip to hack analysis. (#342)
    Adds a code snip for the first 5 incidents.
    Makes the number of generated incidents configurable.
    Makes each the issue name unique by ruleset and rule.
    
    ---------
    
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- hack/add/analysis.sh
+++ hack/add/analysis.sh
@@ -4,12 +4,14 @@ host="${HOST:-localhost:8080}"
 app="${1:-1}"
 nRuleSet="${2:-10}"
 nIssue="${3:-10}"
+nIncident="${4:-25}"
 file="/tmp/analysis.yaml"
 
 echo "Writing report: ${file}"
 echo " Application: ${app}"
 echo " RuleSets: ${nRuleSet}"
 echo " Issues: ${nIssue}"
+echo " Incidents: ${nIncident}"
 
 #
 # Issues
@@ -23,30 +25,49 @@ for i in $(seq 1 ${nIssue})
 do
 echo -n "- ruleset: ruleSet-${r}
   rule: rule-${i}
-  name: Rule-${i}-Violated
-  description: This is a test ${r}/${i}.
+  name: Rule-${r}.${i}-Violated
+  description: This is a test ${r}/${i} violation.
   category: warning
   effort: 10
   labels:
   - konveyor.io/target=RULESET-${r}
   - konveyor.io/source=RULE-${i}
   incidents:
-  - uri: http://thing.com/file:1
-    message: Thing happend line:1
-    facts:
-      factA: 1.A
-      factB: 1.B
-  - uri: http://thing.com/file:2
-    message: Thing happend line:2
-    facts:
-      factA: 1.C
-      factB: 1.D
-  - uri: http://thing.com/file:3
-    message: Thing happend line:3
-    facts:
-      factA: 1.E
-      factB: 1.F
 " >> ${file}
+for n in $(seq 1 ${nIncident})
+do
+echo -n "  - uri: http://thing.com/file/${i}:${n}
+    message: Thing happend line:${n}
+    facts:
+      factA: ${i}-${n}.A
+      factB: ${i}-${n}.B
+" >> ${file}
+if ((${n} < 6)); then echo -n "    codesnip: |
+      public class SwapNumbers {
+          public static void main(String[] args) {
+              float first = 1.20f, second = 2.45f;
+      
+              System.out.println(\"--Before swap--\");
+              System.out.println(\"First number = \" + first);
+              System.out.println(\"Second number = \" + second);
+      
+              // Value of first is assigned to temporary
+              float temporary = first;
+      
+              // Value of second is assigned to first
+              first = second;
+      
+              // Value of temporary (which contains the initial value of first) is assigned to second
+              second = temporary;
+      
+              System.out.println(\"--After swap--\");
+              System.out.println(\"First number = \" + first);
+              System.out.println(\"Second number = \" + second);
+          }
+      }
+" >> ${file}
+fi
+done
 done
 done
 #
@@ -65,6 +86,8 @@ echo -n "dependencies:
   indirect: "true"
   version: 8
 " >> ${file}
+
+echo "Report CREATED"
 
 curl -i -X POST ${host}/applications/${app}/analyses \
   -H 'Content-Type:application/x-yaml' \
