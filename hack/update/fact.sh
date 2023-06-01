171b155af7e5a3f296d04b5853dbb5605f3df121
Jeff Ortel <jortel@redhat.com>
2023-06-01 12:52:12 -0500

    :bug: Addon fact put (#364)
    Fix the addon binding: Body is Fact (not the value).
    Fix Both GORM create and save will add ON CONFLICT clause when the PK is
    set. When source="", the entire composite PK is not considered set.
    
    Backport: 0.2
    
    ---------
    
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- hack/update/fact.sh
+++ hack/update/fact.sh
@@ -2,4 +2,15 @@
 
 host="${HOST:-localhost:8080}"
 
-curl -X PUT ${host}/applications/1/facts/address -d \ '{"street": "Maple","City":"Huntsville","State":"AL"}' | jq -M .
+curl -L -X PUT ${host}/applications/1/facts/address \
+  -H 'Content-Type:application/x-yaml' \
+  -H 'Accept:application/x-yaml' \
+  -d \
+'
+---
+value:
+  street: 1234 Maple St.
+  city: Huntsville
+  state: AL
+  zip: 35763
+'
