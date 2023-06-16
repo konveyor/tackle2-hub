e7950858d7dc4f6b14ffbb24e46ef9b4389b75cf
Samuel Lucidi <sam@samlucidi.com>
2023-06-16 11:26:04 -0400

    Add unique index on Wave name/start/end (#404)
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- migration/v5/model/migrationwave.go
+++ migration/v5/model/migrationwave.go
@@ -4,9 +4,9 @@ import "time"
 
 type MigrationWave struct {
 	Model
-	Name              string
-	StartDate         time.Time
-	EndDate           time.Time
+	Name              string             `gorm:"uniqueIndex:MigrationWaveA"`
+	StartDate         time.Time          `gorm:"uniqueIndex:MigrationWaveA"`
+	EndDate           time.Time          `gorm:"uniqueIndex:MigrationWaveA"`
 	Applications      []Application      `gorm:"constraint:OnDelete:SET NULL"`
 	Stakeholders      []Stakeholder      `gorm:"many2many:MigrationWaveStakeholders;constraint:OnDelete:CASCADE"`
 	StakeholderGroups []StakeholderGroup `gorm:"many2many:MigrationWaveStakeholderGroups;constraint:OnDelete:CASCADE"`
