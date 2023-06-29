eac7781fcbbb62139ec4e4b2779b790d8a2b7852
Sam Lucidi <slucidi@redhat.com>
2023-06-29 15:46:50 -0400

    Prevent RuleSet dependency cycles
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- migration/v6/model/ruleset.go
+++ migration/v6/model/ruleset.go
@@ -1,5 +1,7 @@
 package model
 
+import "gorm.io/gorm"
+
 //
 // RuleSet - Analysis ruleset.
 type RuleSet struct {
@@ -15,6 +17,40 @@ type RuleSet struct {
 	Identity    *Identity
 	Rules       []Rule    `gorm:"constraint:OnDelete:CASCADE"`
 	DependsOn   []RuleSet `gorm:"many2many:RuleSetDependencies;constraint:OnDelete:CASCADE"`
+}
+
+//
+// BeforeUpdate hook to avoid cyclic dependencies.
+func (r *RuleSet) BeforeUpdate(db *gorm.DB) (err error) {
+	seen := make(map[uint]bool)
+	var nextDeps []RuleSet
+	var nextRuleSetIDs []uint
+	for _, dep := range r.DependsOn {
+		nextRuleSetIDs = append(nextRuleSetIDs, dep.ID)
+	}
+	for len(nextRuleSetIDs) != 0 {
+		result := db.Preload("DependsOn").Where("ID IN ?", nextRuleSetIDs).Find(&nextDeps)
+		if result.Error != nil {
+			err = result.Error
+			return
+		}
+		nextRuleSetIDs = nextRuleSetIDs[:0]
+		for _, nextDep := range nextDeps {
+			for _, dep := range nextDep.DependsOn {
+				if seen[dep.ID] {
+					continue
+				}
+				if dep.ID == r.ID {
+					err = DependencyCyclicError{}
+					return
+				}
+				seen[dep.ID] = true
+				nextRuleSetIDs = append(nextRuleSetIDs, dep.ID)
+			}
+		}
+	}
+
+	return
 }
 
 //
