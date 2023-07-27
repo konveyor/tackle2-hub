a7f47cf3abb9fdc9caeaccf5205336d069230030
Jeff Ortel <jortel@redhat.com>
2023-07-27 13:02:12 -0700

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- binding/filter/builder.go
+++ binding/filter/builder.go
@@ -1,118 +1,125 @@
 package filter
 
 import (
-	qf "github.com/konveyor/tackle2-hub/api/filter"
+	//qf "github.com/konveyor/tackle2-hub/api/filter"
 	"reflect"
 	"strconv"
 	"strings"
 )
 
+//
+// Any match any.
 type Any []interface{}
 
+//
+// And match all.
 type And []interface{}
 
+//
+// Filter builder.
 type Filter struct {
 	predicates []string
 }
 
+//
+// String returns string representation.
 func (f *Filter) String() (s string) {
 	s = strings.Join(f.predicates, ",")
 	return
 }
 
-func (f *Filter) Equals(field string, value interface{}) (self *Filter) {
-	f.add(field, value, qf.EQ)
+//
+// And adds a predicate.
+func (f *Filter) And(p *Predicate) *Filter {
+	f.Add(p)
 	return f
 }
 
-func (f *Filter) NotEquals(field string, value interface{}) (self *Filter) {
-	f.add(field, value, qf.NOT)
-	return f
-}
-
-func (f *Filter) Like(field string, value interface{}) *Filter {
-	f.add(field, value, qf.LIKE)
-	return f
-}
-
-func (f *Filter) GreaterThan(field string, value interface{}) (self *Filter) {
-	f.add(field, value, qf.GT)
-	return f
-}
-
-func (f *Filter) LessThan(field string, value interface{}) (self *Filter) {
-	f.add(field, value, qf.GT)
-	return f
-}
-
-func (f *Filter) add(field string, value interface{}, operator byte) (self *Filter) {
-	self = f
-	pv := f.valueOf(value)
-	pv = field + string(operator) + pv
+//
+// Add a predicate.
+func (f *Filter) Add(p *Predicate) *Filter {
 	f.predicates = append(
 		f.predicates,
-		pv)
-	return
+		p.String())
+	return f
 }
 
-func (f *Filter) valueOf(object interface{}) (result string) {
-	kind := reflect.TypeOf(object).Kind()
-	value := reflect.ValueOf(object)
-	switch kind {
-	case reflect.String:
-		result = "'" + value.String() + "'"
-	case reflect.Int,
-		reflect.Int8,
-		reflect.Int16,
-		reflect.Int32,
-		reflect.Int64:
-		n := value.Int()
-		result = strconv.Itoa(int(n))
-	case reflect.Bool:
-		result = strconv.FormatBool(value.Bool())
-	case reflect.Slice:
-		var items []string
-		for i := 0; i < value.Len(); i++ {
-			item := f.valueOf(value.Index(i).Interface())
-			items = append(items, item)
-		}
-		var operator string
-		switch object.(type) {
-		case Any:
-			operator = string(qf.OR)
-		case And:
-			operator = string(qf.AND)
-		default:
-			operator = string(qf.OR)
-		}
-		result = strings.Join(items, operator)
-		result = "(" + result + ")"
-	}
-	return
-}
-
-// filter.Add(Field("name").Equals(dd)
-
+//
+// Field returns a field predicate.
 func Field(name string) (p *Predicate) {
 	p = &Predicate{field: name}
 	return
 }
 
+//
+// Predicate is a filter query predicate.
 type Predicate struct {
 	field    string
-	operator int32
-	value    interface{}
+	operator string
+	value    string
 }
 
+//
+// String returns a string representation of the predicate.
 func (p *Predicate) String() (s string) {
-	pv := p.valueOf(p.value)
-	s = p.field + string(p.operator) + pv
+	s = p.field + string(p.operator) + p.value
 	return
 }
 
-func (p *Predicate) Equals(object interface{}) {
-	p.operator = qf.EQ
+//
+// Equals returns a (=) predicate.
+func (p *Predicate) Equals(object interface{}) *Predicate {
+	p.operator = "="
 	p.value = p.valueOf(object)
+	return p
+}
+
+//
+// NotEquals returns a (!=) predicate.
+func (p *Predicate) NotEquals(object interface{}) *Predicate {
+	p.operator = "!="
+	p.value = p.valueOf(object)
+	return p
+}
+
+//
+// Like returns a (~) predicate.
+func (p *Predicate) Like(object interface{}) *Predicate {
+	p.operator = "~"
+	p.value = p.valueOf(object)
+	return p
+}
+
+//
+// GreaterThan returns a (>) predicate.
+func (p *Predicate) GreaterThan(object interface{}) *Predicate {
+	p.operator = ">"
+	p.value = p.valueOf(object)
+	return p
+}
+
+//
+// GreaterThanEq returns a (>=) predicate.
+func (p *Predicate) GreaterThanEq(object interface{}) *Predicate {
+	p.operator = ">="
+	p.value = p.valueOf(object)
+	return p
+}
+
+//
+// LessThan returns a (<) predicate.
+func (p *Predicate) LessThan(object interface{}) *Predicate {
+	p.operator = "<"
+	p.value = p.valueOf(object)
+	return p
+}
+
+//
+// LessThanEq returns a (<) predicate.
+func (p *Predicate) LessThanEq(object interface{}) *Predicate {
+	p.operator = "<="
+	p.value = p.valueOf(object)
+	return p
 }
 
 func (p *Predicate) valueOf(object interface{}) (result string) {
@@ -139,11 +146,11 @@ func (p *Predicate) valueOf(object interface{}) (result string) {
 		var operator string
 		switch object.(type) {
 		case Any:
-			operator = string(qf.OR)
+			operator = "|"
 		case And:
-			operator = string(qf.AND)
+			operator = ","
 		default:
-			operator = string(qf.OR)
+			operator = "|"
 		}
 		result = strings.Join(items, operator)
 		result = "(" + result + ")"
