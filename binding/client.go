1dfad036e82ac9f7a34d33a7472fd49d420e6a90
Jeff Ortel <jortel@redhat.com>
2023-07-28 10:25:25 -0500

    :sparkles: Binding: add filter builder. (#463)
    Add support for building filter queries.
    
    Example:
    ```
    filter = binding.Filter{}
    filter.And("name").Eq("Elmer")
    filter.And("age").Gt(10)
    filter.And("height").Lt(44)
    filter.And("weight").LtEq(150)
    filter.And("hair").NotEq("blond")
    filter.And("pet").Like("Rov*")
    filter.And("friend").Eq(Any{"Sam","Ed"})
    p := filter.Param()
    client.Get(path, object, p)
    ```
    
    Adds binding.RuleSet.Find() needed by analyzer addon to get rulesets for
    labels.
    Example:
    ```
    filter.And("labels").Eq(labels)
    ```
    
    ---------
    
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- binding/client.go
+++ binding/client.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gin-gonic/gin/binding"
 	liberr "github.com/jortel/go-utils/error"
 	"github.com/konveyor/tackle2-hub/api"
+	qf "github.com/konveyor/tackle2-hub/binding/filter"
 	"io"
 	"mime/multipart"
 	"net"
@@ -34,6 +35,20 @@ const (
 type Param struct {
 	Key   string
 	Value string
+}
+
+//
+// Filter
+type Filter struct {
+	qf.Filter
+}
+
+//
+// Param returns a filter parameter.
+func (r *Filter) Param() (p Param) {
+	p.Key = api.Filter
+	p.Value = r.String()
+	return
 }
 
 //
