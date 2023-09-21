d85051510ee29082f4f2c9183f72a5eab3a0ed3d
Samuel Lucidi <sam@samlucidi.com>
2023-09-21 09:42:56 -0400

    :bug: Fix constraints on archetype tag rels (#490)
    * Adds cascading deletes to archetype tag relationships
    * Surfaces the Review relationship in the archetype resource
    * Removes some dead code
    
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- migration/v9/model/archetype.go
+++ migration/v9/model/archetype.go
@@ -7,8 +7,8 @@ type Archetype struct {
 	Comments          string
 	Review            *Review            `gorm:"constraint:OnDelete:CASCADE"`
 	Assessments       []Assessment       `gorm:"constraint:OnDelete:CASCADE"`
-	CriteriaTags      []Tag              `gorm:"many2many:ArchetypeCriteriaTags"`
-	Tags              []Tag              `gorm:"many2many:ArchetypeTags"`
+	CriteriaTags      []Tag              `gorm:"many2many:ArchetypeCriteriaTags;constraint:OnDelete:CASCADE"`
+	Tags              []Tag              `gorm:"many2many:ArchetypeTags;constraint:OnDelete:CASCADE"`
 	Stakeholders      []Stakeholder      `gorm:"many2many:ArchetypeStakeholders;constraint:OnDelete:CASCADE"`
 	StakeholderGroups []StakeholderGroup `gorm:"many2many:ArchetypeStakeholderGroups;constraint:OnDelete:CASCADE"`
 }
