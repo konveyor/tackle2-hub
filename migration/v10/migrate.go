7dcfe767f03833eb601ce2e5abef297b8275ee0f
Jeff Ortel <jortel@redhat.com>
2023-09-26 15:27:39 -0700

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

---
+++ migration/v10/migrate.go
@@ -0,0 +1,20 @@
+package v10
+
+import (
+	"github.com/jortel/go-utils/logr"
+	"github.com/konveyor/tackle2-hub/migration/v10/model"
+	"gorm.io/gorm"
+)
+
+var log = logr.WithName("migration|v9")
+
+type Migration struct{}
+
+func (r Migration) Apply(db *gorm.DB) (err error) {
+	err = db.AutoMigrate(r.Models()...)
+	return
+}
+
+func (r Migration) Models() []interface{} {
+	return model.All()
+}
---
+++ migration/v10/model/pkg.go
@@ -0,0 +1,87 @@
+package model
+
+import "github.com/konveyor/tackle2-hub/migration/v9/model"
+
+//
+// JSON field (data) type.
+type JSON = []byte
+
+type Model = model.Model
+type Application = model.Application
+type Archetype = model.Archetype
+type Assessment = model.Assessment
+type Bucket = model.Bucket
+type BucketOwner = model.BucketOwner
+type BusinessService = model.BusinessService
+type Dependency = model.Dependency
+type File = model.File
+type Fact = model.Fact
+type Identity = model.Identity
+type Import = model.Import
+type ImportSummary = model.ImportSummary
+type ImportTag = model.ImportTag
+type JobFunction = model.JobFunction
+type MigrationWave = model.MigrationWave
+type Proxy = model.Proxy
+type Questionnaire = model.Questionnaire
+type Review = model.Review
+type Setting = model.Setting
+type RuleSet = model.RuleSet
+type Rule = model.Rule
+type Stakeholder = model.Stakeholder
+type StakeholderGroup = model.StakeholderGroup
+type Tag = model.Tag
+type TagCategory = model.TagCategory
+type Target = model.Target
+type Task = model.Task
+type TaskGroup = model.TaskGroup
+type TaskReport = model.TaskReport
+type Ticket = model.Ticket
+type Tracker = model.Tracker
+type TTL = model.TTL
+type ApplicationTag = model.ApplicationTag
+type DependencyCyclicError = model.DependencyCyclicError
+
+//
+// All builds all models.
+// Models are enumerated such that each are listed after
+// all the other models on which they may depend.
+func All() []interface{} {
+	return []interface{}{
+		Application{},
+		TechDependency{},
+		Incident{},
+		Analysis{},
+		Issue{},
+		Bucket{},
+		BusinessService{},
+		Dependency{},
+		File{},
+		Fact{},
+		Identity{},
+		Import{},
+		ImportSummary{},
+		ImportTag{},
+		JobFunction{},
+		MigrationWave{},
+		Proxy{},
+		Review{},
+		Setting{},
+		RuleSet{},
+		Rule{},
+		Stakeholder{},
+		StakeholderGroup{},
+		Tag{},
+		TagCategory{},
+		Target{},
+		Task{},
+		TaskGroup{},
+		TaskReport{},
+		Ticket{},
+		Tracker{},
+		ApplicationTag{},
+		Questionnaire{},
+		Assessment{},
+		Archetype{},
+	}
+}
