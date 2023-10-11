54849ba24bb531afc3ccc674031ddba7e85e0222
Jeff Ortel <jortel@redhat.com>
2023-10-11 09:22:05 -0500

    :bug: Fix HTML analysis report. (#513)
    The HTML report (tarball) is composed/streamed by the hub in steps:
    1. Add the static report directory (template).
    2. Add the composed `output.js` file which contains the analysis data.
    
    Using this method, the ReportWriter was adding the `output.js` (entry)
    twice in the tar. First from the static report (template) and the second
    generated by the hub.
    The tar command line seems to deal with this by overwriting the file.
    However the _extract_ functionality used by browsers ignores the 2nd
    occurrence.
    
    The fix is to filter out the 1st output.js when using
    tar.Writer.AddDir().
    
    The `Filter` in the `tar` package only supported _include_ filtering.
    This PR updates the `tar.Filter` to support both _included_ and
    _excluded_ patterns.
    
    Refitted and tested the /bucket.
    
    ---------
    
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- tar/filter.go
+++ tar/filter.go
@@ -6,27 +6,104 @@ import (
 )
 
 //
+// NewFilter returns a filter.
+func NewFilter(root string) (f Filter) {
+	f = Filter{Root: root}
+	return
+}
+
+//
 // Filter supports glob-style filtering.
 type Filter struct {
-	Root    string
-	Pattern string
-	cache   map[string]bool
+	included FilterSet
+	excluded FilterSet
+	Root     string
 }
 
 //
 // Match determines if path matches the filter.
 func (r *Filter) Match(path string) (b bool) {
-	if r.Pattern == "" {
-		b = true
+	r.included.root = r.Root
+	r.excluded.root = r.Root
+	if r.included.Len() > 0 {
+		included := r.included.Match(path)
+		if !included {
+			return
+		}
+	}
+	b = true
+	if r.excluded.Len() > 0 {
+		excluded := r.excluded.Match(path)
+		if excluded {
+			b = false
+			return
+		}
+	}
+	return
+}
+
+//
+// Include adds included patterns.
+// Empty ("") patterns are ignored.
+func (r *Filter) Include(patterns ...string) {
+	r.included.Add(patterns...)
+}
+
+//
+// Exclude adds excluded patterns.
+// Empty ("") patterns are ignored.
+func (r *Filter) Exclude(patterns ...string) {
+	r.excluded.Add(patterns...)
+}
+
+//
+// FilterSet is a collection of filter patterns.
+type FilterSet struct {
+	root     string
+	patterns []string
+	cache    map[string]bool
+}
+
+//
+// Match returns true when the path matches.
+func (r *FilterSet) Match(path string) (match bool) {
+	r.build()
+	_, match = r.cache[path]
+	return
+}
+
+//
+// Add pattern.
+// Empty ("") patterns are ignored.
+func (r *FilterSet) Add(patterns ...string) {
+	for _, p := range patterns {
+		if p == "" {
+			continue
+		}
+		r.cache = nil
+		r.patterns = append(
+			r.patterns,
+			p)
+	}
+}
+
+//
+// Len returns number of patterns.
+func (r *FilterSet) Len() (n int) {
+	return len(r.patterns)
+}
+
+//
+// build populates the cache as needed.
+func (r *FilterSet) build() {
+	if r.cache != nil {
 		return
 	}
-	if r.cache == nil {
-		r.cache = map[string]bool{}
-		matches, _ := filepath.Glob(pathlib.Join(r.Root, r.Pattern))
+	r.cache = make(map[string]bool)
+	for i := range r.patterns {
+		matches, _ := filepath.Glob(pathlib.Join(r.root, r.patterns[i]))
 		for _, p := range matches {
 			r.cache[p] = true
 		}
 	}
-	_, b = r.cache[path]
-	return
 }
