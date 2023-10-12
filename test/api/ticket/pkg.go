aae707c97e60308ae1b299d3ad071e377d3b5544
Yash Khare <yash2010118@akgec.ac.in>
2023-10-12 12:29:29 +0530

    :seedling: add ticket API test (#477)
    Add the ticket API Test
    
    ---------
    
    Signed-off-by: Yash Khare <yash2010118@akgec.ac.in>

---
+++ test/api/ticket/pkg.go
@@ -0,0 +1,31 @@
+package ticket
+
+import (
+	"github.com/konveyor/tackle2-hub/binding"
+	"github.com/konveyor/tackle2-hub/test/api/client"
+)
+
+var (
+	RichClient  *binding.RichClient
+	Ticket      binding.Ticket
+	Tracker     binding.Tracker
+	Identity    binding.Identity
+	Application binding.Application
+)
+
+func init() {
+	// Prepare RichClient and login to Hub API (configured from env variables).
+	RichClient = client.PrepareRichClient()
+
+	// Shortcut for Ticket-related RichClient methods.
+	Ticket = RichClient.Ticket
+
+	// Shortcut for Tracker-related RichClient methods.
+	Tracker = RichClient.Tracker
+
+	// Shortcut for Identity-related RichClient methods.
+	Identity = RichClient.Identity
+
+	// Shortcut for Application-related RichClient methods.
+	Application = RichClient.Application
+}
