7357d1b9e093dd2664bfc4cb751dcb30a785559f
Jeff Ortel <jortel@redhat.com>
2023-10-19 12:10:39 -0700

    :ghost: add hack to generate jwt tokens for builtin auth.
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

---
+++ hack/jwt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Usage: hubtoken.sh <key>
+#
+key=$1
+hexKey=$(echo -n "key" \
+  | xxd -p \
+  | tr -d '\n')
+header='{"alg":"HS512","typ":"JWT"}'
+payload='{"scope":"*:*","user":"operator"}'
+headerStr=$(echo -n ${header} \
+  | base64 -w 0 \
+  | sed s/\+/-/g \
+  | sed 's/\//_/g' \
+  | sed -E s/=+$//)
+payloadStr=$(echo -n ${payload} \
+  | base64 -w 0 \
+  | sed s/\+/-/g \
+  | sed 's/\//_/g' \
+  | sed -E s/=+$//)
+signStr=$(echo -n "${headerStr}.${payloadStr}" \
+  | openssl dgst -sha512 -mac HMAC -macopt hexkey:${hexKey} -binary \
+  | base64  -w 0 \
+  | sed s/\+/-/g \
+  | sed 's/\//_/g' \
+  | sed -E s/=+$//)
+token="${headerStr}.${payloadStr}.${signStr}"
+echo "${token}"
