98f338c70bf6ac4668f21537f5064cb12454dca8
Jeff Ortel <jortel@redhat.com>
2023-10-25 14:12:19 -0500

    :sparkles: Add Target.provider. (#541)
    closes #539
    
    ---------
    
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

---
+++ migration/v11/migrate.go
@@ -0,0 +1,20 @@
+package v11
+
+import (
+	"github.com/jortel/go-utils/logr"
+	"github.com/konveyor/tackle2-hub/migration/v11/model"
+	"gorm.io/gorm"
+)
+
+var log = logr.WithName("migration|v10")
+
+type Migration struct{}
+
+func (r Migration) Apply(db *gorm.DB) (err error) {
+	err = db.AutoMigrate(r.Models()...)
+	return
+}
+
+func (r Migration) Models() []interface{} {
+	return model.All()
+}
