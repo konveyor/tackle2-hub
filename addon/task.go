0fac993b56484d4f3512807dad9f67acd69b49c5
Jeff Ortel <jortel@redhat.com>
2023-12-13 07:47:01 -0800

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- addon/task.go
+++ addon/task.go
@@ -154,15 +154,15 @@ func (h *Task) Activity(entry string, v ...interface{}) {
 //
 // Attach ensures the file is attached to the report.
 func (h *Task) Attach(f *api.File) {
-	Log.Info(
-		"Addon attached: %s",
-		"path",
-		f.Path)
 	for _, ref := range h.report.Files {
 		if ref.ID == f.ID {
 			return
 		}
 	}
+	Log.Info(
+		"Addon attached: %s",
+		"path",
+		f.Path)
 	h.report.Files = append(
 		h.report.Files,
 		api.Ref{
