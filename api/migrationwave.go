d55494535bfb0a33ec0662bff8a36edabd771321
Sam Lucidi <slucidi@redhat.com>
2024-02-21 11:17:35 -0500

    waves: require start and end date, end > start
    Fixes https://issues.redhat.com/browse/MTA-1726
    
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- api/migrationwave.go
+++ api/migrationwave.go
@@ -182,8 +182,8 @@ func (h MigrationWaveHandler) Delete(ctx *gin.Context) {
 type MigrationWave struct {
 	Resource          `yaml:",inline"`
 	Name              string    `json:"name"`
-	StartDate         time.Time `json:"startDate" yaml:"startDate"`
-	EndDate           time.Time `json:"endDate" yaml:"endDate"`
+	StartDate         time.Time `json:"startDate" yaml:"startDate" binding:"required"`
+	EndDate           time.Time `json:"endDate" yaml:"endDate" binding:"required,gtfield=StartDate"`
 	Applications      []Ref     `json:"applications"`
 	Stakeholders      []Ref     `json:"stakeholders"`
 	StakeholderGroups []Ref     `json:"stakeholderGroups" yaml:"stakeholderGroups"`
