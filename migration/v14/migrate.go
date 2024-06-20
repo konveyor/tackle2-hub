65ced3942be684b1c16fdad3d80acac75b6a8015
Jeff Ortel <jortel@redhat.com>
2024-06-20 12:08:15 -0500

    :sparkles: Add mvn:// prefix to application.binary. (#668)
    Required by: https://github.com/konveyor/tackle2-addon-analyzer/pull/97
    
    ---------
    
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- migration/v14/migrate.go
+++ migration/v14/migrate.go
@@ -1,17 +1,39 @@
 package v14
 
 import (
+	"strings"
+
 	"github.com/jortel/go-utils/logr"
 	"github.com/konveyor/tackle2-hub/migration/v14/model"
 	"gorm.io/gorm"
 )
 
-var log = logr.WithName("migration|v13")
+var log = logr.WithName("migration|v14")
 
 type Migration struct{}
 
 func (r Migration) Apply(db *gorm.DB) (err error) {
 	err = db.AutoMigrate(r.Models()...)
+	if err != nil {
+		return
+	}
+	// add mvn:// prefix.
+	prefix := "mvn://"
+	var list []*model.Application
+	err = db.Find(&list).Error
+	if err != nil {
+		return
+	}
+	for _, m := range list {
+		if m.Binary == "" || strings.HasPrefix(m.Binary, prefix) {
+			continue
+		}
+		m.Binary = prefix + m.Binary
+		err = db.Save(m).Error
+		if err != nil {
+			return
+		}
+	}
 	return
 }
 
