6851316342a3395fa1eacb834fc24020d1bf8918
Shawn Hurley <shawn@hurley.page>
2024-07-03 18:22:04 -0400

    :ghost: Revert "Revert ":sparkles: Ensure analyzer tasks created prior to upgrade have kind assigned."" (#695)
    Reverts konveyor/tackle2-hub#686

--- migration/v14/migrate.go
+++ migration/v14/migrate.go
@@ -17,7 +17,20 @@ func (r Migration) Apply(db *gorm.DB) (err error) {
 	if err != nil {
 		return
 	}
-	// add mvn:// prefix.
+	err = r.mavenPrefix(db)
+	if err != nil {
+		return
+	}
+	err = r.taskKind(db)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// mavenPrefix ensures the Application.Binary which are maven
+// coordinates have the mvn:// prefix added in 0.5.
+func (r Migration) mavenPrefix(db *gorm.DB) (err error) {
 	prefix := "mvn://"
 	var list []*model.Application
 	err = db.Find(&list).Error
@@ -29,6 +42,28 @@ func (r Migration) Apply(db *gorm.DB) (err error) {
 			continue
 		}
 		m.Binary = prefix + m.Binary
+		err = db.Save(m).Error
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+// taskKind ensures tasks have a kind.
+// In 0.5 task (kinds) added. A task named `analyzer` is
+// installed by the operator.
+func (r Migration) taskKind(db *gorm.DB) (err error) {
+	kind := "analyzer"
+	var list []*model.Task
+	err = db.Find(&list).Error
+	if err != nil {
+		return
+	}
+	for _, m := range list {
+		if m.Addon == kind && m.Kind == "" {
+			m.Kind = kind
+		}
 		err = db.Save(m).Error
 		if err != nil {
 			return
