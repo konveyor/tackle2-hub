54e88d42d7cacd35256dbbcb6ceef5198cbf9aa9
Jeff Ortel <jortel@redhat.com>
2024-07-08 20:07:23 -0700

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- database/driver.go
+++ database/driver.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"database/sql/driver"
+	"strings"
 	"sync"
 
 	"github.com/mattn/go-sqlite3"
@@ -45,6 +46,7 @@ func (d *Driver) Driver() driver.Driver {
 type Conn struct {
 	mutex   *sync.Mutex
 	wrapped driver.Conn
+	tx      driver.Tx
 }
 
 func (c *Conn) Ping(ctx context.Context) (err error) {
@@ -68,6 +70,10 @@ func (c *Conn) IsValid() (b bool) {
 }
 
 func (c *Conn) QueryContext(ctx context.Context, query string, args []driver.NamedValue) (r driver.Rows, err error) {
+	if c.tx == nil {
+		c.mutex.Lock()
+		defer c.mutex.Unlock()
+	}
 	if p, cast := c.wrapped.(driver.QueryerContext); cast {
 		r, err = p.QueryContext(ctx, query, args)
 	}
@@ -78,10 +84,26 @@ func (c *Conn) PrepareContext(ctx context.Context, query string) (s driver.Stmt,
 	if p, cast := c.wrapped.(driver.ConnPrepareContext); cast {
 		s, err = p.PrepareContext(ctx, query)
 	}
+	if err != nil {
+		return
+	}
+	stmtLocked := c.stmtLocked(query)
+	s = &Stmt{
+		mutex:   c.mutex,
+		locked:  stmtLocked,
+		wrapped: s,
+	}
+	if stmtLocked {
+		c.mutex.Lock()
+	}
 	return
 }
 
 func (c *Conn) ExecContext(ctx context.Context, query string, args []driver.NamedValue) (r driver.Result, err error) {
+	if c.tx == nil {
+		c.mutex.Lock()
+		defer c.mutex.Unlock()
+	}
 	if p, cast := c.wrapped.(driver.ExecerContext); cast {
 		r, err = p.ExecContext(ctx, query, args)
 	}
@@ -101,12 +123,25 @@ func (c *Conn) BeginTx(ctx context.Context, opts driver.TxOptions) (tx driver.Tx
 		mutex:   c.mutex,
 		wrapped: tx,
 	}
+	c.tx = tx
 	c.mutex.Lock()
 	return
 }
 
 func (c *Conn) Prepare(query string) (s driver.Stmt, err error) {
 	s, err = c.wrapped.Prepare(query)
+	if err != nil {
+		return
+	}
+	stmtLocked := c.stmtLocked(query)
+	s = &Stmt{
+		mutex:   c.mutex,
+		locked:  stmtLocked,
+		wrapped: s,
+	}
+	if stmtLocked {
+		c.mutex.Lock()
+	}
 	return
 }
 
@@ -124,6 +159,22 @@ func (c *Conn) Begin() (tx driver.Tx, err error) {
 		mutex:   c.mutex,
 		wrapped: tx,
 	}
+	c.tx = tx
+	c.mutex.Lock()
+	return
+}
+
+func (c *Conn) stmtLocked(query string) (matched bool) {
+	if c.tx != nil || query == "" {
+		return
+	}
+	query = strings.ToUpper(query)
+	action := strings.Fields(query)[0]
+	action = strings.ToUpper(action)
+	matched = action == "CREATE" ||
+		action == "INSERT" ||
+		action == "UPDATE" ||
+		action == "DELETE"
 	return
 }
 
@@ -144,5 +195,46 @@ func (tx *Tx) Rollback() (err error) {
 		tx.mutex.Unlock()
 	}()
 	err = tx.wrapped.Rollback()
+	return
+}
+
+type Stmt struct {
+	mutex   *sync.Mutex
+	wrapped driver.Stmt
+	locked  bool
+}
+
+func (s *Stmt) Close() (err error) {
+	if s.locked {
+		s.mutex.Unlock()
+	}
+	err = s.wrapped.Close()
+	return
+}
+func (s *Stmt) NumInput() (n int) {
+	n = s.wrapped.NumInput()
+	return
+}
+func (s *Stmt) Exec(args []driver.Value) (r driver.Result, err error) {
+	r, err = s.wrapped.Exec(args)
+	return
+}
+
+func (s *Stmt) ExecContext(ctx context.Context, args []driver.NamedValue) (r driver.Result, err error) {
+	if s, cast := s.wrapped.(driver.StmtExecContext); cast {
+		r, err = s.ExecContext(ctx, args)
+	}
+	return
+}
+
+func (s *Stmt) Query(args []driver.Value) (r driver.Rows, err error) {
+	r, err = s.wrapped.Query(args)
+	return
+}
+
+func (s *Stmt) QueryContext(ctx context.Context, args []driver.NamedValue) (r driver.Rows, err error) {
+	if p, cast := s.wrapped.(driver.StmtQueryContext); cast {
+		r, err = p.QueryContext(ctx, args)
+	}
 	return
 }
