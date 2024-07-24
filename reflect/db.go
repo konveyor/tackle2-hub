80b19ac64ff922f29b8b2eedfe7540ae6e69ed78
Jeff Ortel <jortel@redhat.com>
2024-07-23 19:53:58 -0700

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- reflect/db.go
+++ reflect/db.go
@@ -4,7 +4,7 @@ import (
 	"gorm.io/gorm"
 )
 
-// Select returns DB.Select().
+// Select returns DB.Select() with validated fields.
 func Select(in *gorm.DB, m any, fields ...string) (out *gorm.DB) {
 	fields, err := HasFields(m, fields...)
 	out = in.Select(fields)
@@ -15,7 +15,7 @@ func Select(in *gorm.DB, m any, fields ...string) (out *gorm.DB) {
 	return
 }
 
-// Omit returns DB.Omit().
+// Omit returns DB.Omit() with validated fields.
 func Omit(in *gorm.DB, m any, fields ...string) (out *gorm.DB) {
 	fields, err := HasFields(m, fields...)
 	out = in.Omit(fields...)
