4a2e5ac7468afed21adf8994c904ff7553069416
Jeff Ortel <jortel@redhat.com>
2024-08-20 14:22:53 -0700

    Fix database-locked error.
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- database/db_test.go
+++ database/db_test.go
@@ -7,9 +7,10 @@ import (
 	"testing"
 
 	"github.com/konveyor/tackle2-hub/model"
+	"k8s.io/utils/env"
 )
 
-var N = 800
+var N, _ = env.GetInt("TEST_CONCURRENT", 10)
 
 func TestConcurrent(t *testing.T) {
 	Settings.DB.Path = "/tmp/concurrent.db"
@@ -25,9 +26,25 @@ func TestConcurrent(t *testing.T) {
 			for n := 0; n < N; n++ {
 				v, _ := json.Marshal(fmt.Sprintf("Test-%d", n))
 				m := &model.Setting{Key: fmt.Sprintf("key-%d-%d", id, n), Value: v}
+				fmt.Printf("(%.4d) CREATE: %.4d\n", id, n)
 				uErr := db.Create(m).Error
 				if uErr != nil {
 					panic(uErr)
+				}
+				for i := 0; i < 10; i++ {
+					fmt.Printf("(%.4d) BEGIN: %.4d/%.4d\n", id, n, i)
+					tx := db.Begin()
+					fmt.Printf("(%.4d) FIRST: %.4d/%.4d\n", id, n, i)
+					uErr = tx.First(m).Error
+					if uErr != nil {
+						panic(uErr)
+					}
+					fmt.Printf("(%.4d) SAVE: %.4d/%.4d\n", id, n, i)
+					uErr = tx.Save(m).Error
+					if uErr != nil {
+						panic(uErr)
+					}
+					tx.Commit()
 				}
 			}
 			dq <- id
