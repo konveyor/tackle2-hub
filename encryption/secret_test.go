14544701b70c272f69cb542f7e0e2d1705339142
Jeff Ortel <jortel@redhat.com>
2025-04-29 08:44:48 -0500

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- encryption/secret_test.go
+++ encryption/secret_test.go
@@ -69,3 +69,65 @@ func TestSecret(t *testing.T) {
 	err = secret.Decrypt(mp)
 	g.Expect(err).To(gomega.BeNil())
 }
+
+func TestPackage(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	Settings.Passphrase = "TEST"
+
+	//
+	// string
+	password := "broker"
+	err := Encrypt(&password)
+	g.Expect(err).To(gomega.BeNil())
+	err = Decrypt(&password)
+	g.Expect(err).To(gomega.BeNil())
+
+	//
+	// struct
+	object := struct {
+		Name     string
+		User     string `secret:"aes"`
+		Password string `secret:"aes"`
+		Age      int
+		List     []string       `secret:"aes"`
+		Map      map[string]any `secret:"aes"`
+	}{
+		Name:     "elmer",
+		User:     "rfudd@warnerbrothers.com",
+		Password: "rabbit-slaye",
+		Age:      52,
+		List: []string{
+			"A",
+			"B",
+			"C",
+		},
+		Map: map[string]any{
+			"k0": "v0",
+			"k1": "v1",
+			"k2": 2,
+		},
+	}
+	err = Encrypt(&object)
+	g.Expect(err).To(gomega.BeNil())
+	err = Decrypt(&object)
+	g.Expect(err).To(gomega.BeNil())
+
+	//
+	// map
+	mp := map[string]any{
+		"name":     "elmer",
+		"user":     "rfudd@warnerbrothers.com",
+		"password": "rabbit-slaye",
+		"age":      52,
+		"list": []string{
+			"A",
+			"B",
+			"C",
+		},
+	}
+	err = Encrypt(mp)
+	g.Expect(err).To(gomega.BeNil())
+	err = Decrypt(mp)
+	g.Expect(err).To(gomega.BeNil())
+}
