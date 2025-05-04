e7c6c56707505d38ca6d86e37b3f43281c623c99
Jeff Ortel <jortel@redhat.com>
2025-05-04 10:41:04 -0500

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- hack/add/review.sh
+++ hack/add/review.sh
@@ -2,32 +2,22 @@
 
 host="${HOST:-localhost:8080}"
 
-curl -X POST ${host}/reviews -d \
-'{
-    "businessCriticality": 4,
-    "effortEstimate": "large",
-    "proposedAction": "proceed",
-    "workPriority": 1,
-    "comments": "This is good.",
-    "application": {"id":1}
-}' | jq -M .
+# id (default: 1)
+# pass Zero(0) for system assigned.
+id="${1:-1}"
+application="${2:-1}"
 
-curl -X POST ${host}/reviews -d \
-'{
-    "businessCriticality": 4,
-    "effortEstimate": "small",
-    "proposedAction": "rehost",
-    "workPriority": 1,
-    "comments": "This is different.",
-    "application": {"id":2}
-}' | jq -M .
-
-curl -X POST ${host}/reviews -d \
-'{
-    "businessCriticality": 4,
-    "effortEstimate": "extra_large",
-    "proposedAction": "repurchase",
-    "workPriority": 1,
-    "comments": "This is hard.",
-    "application": {"id":3}
-}' | jq -M .
+curl -X POST ${host}/reviews \
+  -H 'Content-Type:application/x-yaml' \
+  -H 'Accept:application/x-yaml' \
+-d \
+"
+id: ${id}
+businessCriticality: 4
+effortEstimate: large
+proposedAction: proceed
+workPriority: 1
+comments: This is good.
+application:
+  id: ${application}
+"
