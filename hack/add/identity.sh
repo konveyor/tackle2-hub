4f912ed9e6faf8d196f77ea794d4336687cf84d2
Jeff Ortel <jortel@redhat.com>
2025-05-04 08:14:46 -0500

    identity
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- hack/add/identity.sh
+++ hack/add/identity.sh
@@ -2,24 +2,16 @@
 
 host="${HOST:-localhost:8080}"
 
-curl -X POST ${host}/identities -d \
-'{
-    "kind": "source",
-    "name":"test-git",
-    "description": "Forklift",
-    "user": "userA",
-    "password": "passwordA",
-    "key": "keyA",
-    "settings": "settingsA"
-}' | jq -M .
-
-curl -X POST ${host}/identities -d \
-'{
-    "kind": "maven",
-    "name":"test-mvn",
-    "description": "Forklift",
-    "user": "userA",
-    "password": "passwordA",
-    "key": "keyA",
-    "settings": "settingsA"
-}' | jq -M .
+curl -X POST ${host}/identities \
+  -H 'Content-Type:application/x-yaml' \
+  -H 'Accept:application/x-yaml' \
+ -d \
+'
+kind: source
+name: test-git
+description: Test Description
+user: userA
+password: passwordA
+key: keyA
+settings: settingsA
+'
