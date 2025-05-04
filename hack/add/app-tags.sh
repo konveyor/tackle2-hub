6004828263db77dfd6d4d399140b8d1ea08d154d
Jeff Ortel <jortel@redhat.com>
2025-05-04 11:54:51 -0500

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- hack/add/app-tags.sh
+++ hack/add/app-tags.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 host="${HOST:-localhost:8080}"
+appid="${1:-1}"
 
 # Replace analysis tags.
-curl -X PUT "${host}/applications/1/tags?source=analysis" \
+curl -X PUT "${host}/applications/${appid}/tags?source=analysis" \
   -H 'Content-Type:application/x-yaml' \
   -H 'Accept:application/x-yaml' \
  -d \
@@ -15,7 +16,7 @@ curl -X PUT "${host}/applications/1/tags?source=analysis" \
 '
 
 # add user tag.
-curl -X POST "${host}/applications/1/tags" \
+curl -X POST "${host}/applications/${appid}/tags" \
    -H 'Content-Type:application/x-yaml' \
    -H 'Accept:application/x-yaml' \
   -d \
