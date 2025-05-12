4eb41cb08668dc80fa13e5f3514a910453756d13
Jeff Ortel <jortel@redhat.com>
2025-05-12 11:46:56 -0500

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

---
+++ test/api/platform/samples.go
@@ -0,0 +1,14 @@
+package manifest
+
+import (
+	"github.com/konveyor/tackle2-hub/api"
+)
+
+// Set of valid resources for tests and reuse.
+var (
+	Base = api.Platform{
+		Kind: "base",
+		Name: "Test",
+		URL:  "http://localhost",
+	}
+)
