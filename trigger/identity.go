85be8e24615dd9a9e73e1eb720b2c3b42e54a375
Jeff Ortel <jortel@redhat.com>
2025-05-30 09:06:59 -0500

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- trigger/identity.go
+++ trigger/identity.go
@@ -30,41 +30,43 @@ func (r *Identity) Updated(m *model.Identity) (err error) {
 	if err != nil {
 		return
 	}
-	var appList []model.Application
-	err = r.DB.Find(&appList, affected).Error
-	if err != nil {
-		return
-	}
-	for i := range appList {
-		err = tr.Updated(&m.Applications[i])
+	for _, batch := range affected {
+		var appList []model.Application
+		err = r.DB.Find(&appList, batch).Error
 		if err != nil {
 			return
+		}
+		for i := range appList {
+			err = tr.Updated(&appList[i])
+			if err != nil {
+				return
+			}
 		}
 	}
 	return
 }
 
-// affected returns a list of affected application ids.
+// affected returns the affected application ids.
 // An application is affected when:
 //- the changed identity is directly associated
 //- the changed identity is the default (for the kind) and an
 //  application does not have an identity of the same kind
 //  directly associated.
-func (r *Identity) affected(changed *model.Identity) (appIds []uint, err error) {
+func (r *Identity) affected(changed *model.Identity) (appIds [][]uint, err error) {
 	type M struct {
-		ID      uint
+		AppId   uint
+		Id      uint
 		Kind    string
 		Default bool
-		AppId   uint
 	}
 	db := r.DB.Select(
-		"i.ID ID",
+		"a.ID AppId",
+		"i.ID Id",
 		"i.Kind Kind",
-		"i.`Default` `Default`",
-		"j.ApplicationID AppId")
-	db = db.Table("Identity i")
-	db = db.Joins("LEFT JOIN ApplicationIdentity j ON i.ID = j.IdentityID")
-	db = db.Where("i.Kind", changed.Kind)
+		"i.`Default` `Default`")
+	db = db.Table("Application a")
+	db = db.Joins("LEFT JOIN ApplicationIdentity j ON j.ApplicationID = a.ID")
+	db = db.Joins("LEFT JOIN Identity i ON i.ID = j.IdentityID AND i.Kind = ?", changed.Kind)
 	cursor, err := db.Rows()
 	if err != nil {
 		return
@@ -83,25 +85,37 @@ func (r *Identity) affected(changed *model.Identity) (appIds []uint, err error) 
 	}
 	direct := make(map[uint]uint)
 	for _, m2 := range records {
-		if m2.AppId > 0 {
-			direct[m2.AppId] = m2.ID
+		if m2.Id > 0 {
+			direct[m2.AppId] = m2.Id
 		}
 	}
 	indirect := make(map[uint]uint)
-	for _, m2 := range records {
-		if m2.Default {
+	if changed.Default {
+		for _, m2 := range records {
 			_, hasDirect := direct[m2.AppId]
-			if hasDirect || m2.AppId == 0 {
-				continue
+			if !hasDirect {
+				indirect[m2.AppId] = m2.Id
 			}
-			indirect[m2.AppId] = m2.ID
 		}
 	}
+	batch := make([]uint, 0, 100)
+	add := func(id uint) {
+		if len(batch) == cap(batch) {
+			appIds = append(appIds, batch)
+			batch = make([]uint, 0, cap(batch))
+		}
+		batch = append(batch, id)
+	}
+	defer func() {
+		if len(batch) > 0 {
+			appIds = append(appIds, batch)
+		}
+	}()
 	for appId, _ := range direct {
-		appIds = append(appIds, appId)
+		add(appId)
 	}
 	for appId, _ := range indirect {
-		appIds = append(appIds, appId)
+		add(appId)
 	}
 	return
 }
