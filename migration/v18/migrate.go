e54a74ec55656d4af2adca74281f4ee1ab38245d
Jeff Ortel <jortel@redhat.com>
2025-06-10 11:27:12 -0500

    Issue renamed: insight.
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- migration/v18/migrate.go
+++ migration/v18/migrate.go
@@ -1,16 +1,23 @@
 package v18
 
 import (
-	"github.com/jortel/go-utils/logr"
+	v17 "github.com/konveyor/tackle2-hub/migration/v17/model"
 	"github.com/konveyor/tackle2-hub/migration/v18/model"
 	"gorm.io/gorm"
 )
 
-var log = logr.WithName("migration|v17")
-
 type Migration struct{}
 
 func (r Migration) Apply(db *gorm.DB) (err error) {
+	migrator := db.Migrator()
+	err = migrator.DropIndex(v17.Issue{}, "issueA")
+	if err != nil {
+		return
+	}
+	err = migrator.RenameTable(v17.Issue{}, model.Insight{})
+	if err != nil {
+		return
+	}
 	err = db.AutoMigrate(r.Models()...)
 	return
 }
