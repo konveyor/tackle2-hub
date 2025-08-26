konveyor/tackle2-hub

commit 3a3229767c0539e54c4a3cfcc7b41b9acf22c9f7

:bug: Improved application LIST performance. (#789)
Currently, the application List endpoint performs a query for each
application to fetch the tags. This does not scale well. This PR fetches
all of the tags into a map keyed by application ID. Pretty sure this
must be similar behavior to the un-correlated subquery done by GORM when
pre-loading.

The ApplicationTag (join) models are small so not worried about memory
footprint.

This reduced the performance (on my minikube with SSD) from 1.8s =>
~700ms with 5k applications which is a 60% performance increase. I would
expect a more significant impact on cluster hdd backed file systems.

Roughly related to: https://issues.redhat.com/browse/MTA-4165

---------

Signed-off-by: Jeff Ortel <jortel@redhat.com>

api/application.go
@@ -110,15 +110,11 @@ func (h ApplicationHandler) Get(ctx *gin.Context) {
 		_ = ctx.Error(result.Error)
 		return
 	}
-
-	tags := []model.ApplicationTag{}
-	db = h.preLoad(h.DB(ctx), clause.Associations)
-	result = db.Find(&tags, "ApplicationID = ?", id)
-	if result.Error != nil {
-		_ = ctx.Error(result.Error)
+	tagMap, err := h.tagMap(ctx, []model.Application{*m})
+	if err != nil {
+		_ = ctx.Error(err)
 		return
 	}
-
 	questionnaire, err := assessment.NewQuestionnaireResolver(h.DB(ctx))
 	if err != nil {
 		_ = ctx.Error(err)
@@ -132,7 +128,7 @@ func (h ApplicationHandler) Get(ctx *gin.Context) {
 	}
 	resolver := assessment.NewApplicationResolver(m, tagsResolver, membership, questionnaire)
 	r := Application{}
-	r.With(m, tags)
+	r.With(m, tagMap[m.ID])
 	err = r.WithResolver(resolver)
 	if err != nil {
 		_ = ctx.Error(err)
@@ -156,7 +152,11 @@ func (h ApplicationHandler) List(ctx *gin.Context) {
 		_ = ctx.Error(result.Error)
 		return
 	}
-
+	tagMap, err := h.tagMap(ctx, list)
+	if err != nil {
+		_ = ctx.Error(err)
+		return
+	}
 	questionnaire, err := assessment.NewQuestionnaireResolver(h.DB(ctx))
 	if err != nil {
 		_ = ctx.Error(err)
@@ -168,19 +168,12 @@ func (h ApplicationHandler) List(ctx *gin.Context) {
 		_ = ctx.Error(err)
 		return
 	}
-
 	resources := []Application{}
 	for i := range list {
-		tags := []model.ApplicationTag{}
-		db = h.preLoad(h.DB(ctx), clause.Associations)
-		result = db.Find(&tags, "ApplicationID = ?", list[i].ID)
-		if result.Error != nil {
-			_ = ctx.Error(result.Error)
-			return
-		}
+		m := &list[i]
 		resolver := assessment.NewApplicationResolver(&list[i], tagsResolver, membership, questionnaire)
 		r := Application{}
-		r.With(&list[i], tags)
+		r.With(m, tagMap[m.ID])
 		err = r.WithResolver(resolver)
 		if err != nil {
 			_ = ctx.Error(err)
@@ -1106,6 +1099,29 @@ func (h ApplicationHandler) AssessmentCreate(ctx *gin.Context) {
 
 	r.With(m)
 	h.Respond(ctx, http.StatusCreated, r)
+}
+
+// tagMap returns a map of applicationTags indexed by application id.
+func (h *ApplicationHandler) tagMap(
+	ctx *gin.Context,
+	applications []model.Application) (mp map[uint][]model.ApplicationTag, err error) {
+	ids := []uint{}
+	for i := range applications {
+		m := &applications[i]
+		ids = append(ids, m.ID)
+	}
+	mp = make(map[uint][]model.ApplicationTag)
+	list := []model.ApplicationTag{}
+	db := h.DB(ctx)
+	db = db.Joins("Tag")
+	err = db.Find(&list, "ApplicationID", ids).Error
+	if err != nil {
+		return
+	}
+	for _, m := range list {
+		mp[m.ApplicationID] = append(mp[m.ApplicationID], m)
+	}
+	return
 }
 
 // Application REST resource.

commit 790ea7d8973a69c72fa15466779950c4b1bef624

:bug: fix assessment risk and confidence when listing applications. (#892)
The _resolvers_ expect the `Application.Assessments[].Sections` to be
populated.
This works for Get() because it uses db.Fetch() with
clause._Associations_ which fetches the entire nested Assessment object.
List() uses a a custom join for performance and memory footprint.

This is a regression introduced by
https://github.com/konveyor/tackle2-hub/pull/789 RE: application list
performance and memory footprint. OPTIMIZATION.

closes #891

Signed-off-by: Jeff Ortel <jortel@redhat.com>

api/application.go
@@ -188,24 +188,25 @@ func (h ApplicationHandler) List(ctx *gin.Context) {
 
 	type M struct {
 		*model.Application
-		IdentityId      uint
-		IdentityName    string
-		ServiceId       uint
-		ServiceName     string
-		OwnerId         uint
-		OwnerName       string
-		ContributorId   uint
-		ContributorName string
-		WaveId          uint
-		WaveName        string
-		PlatformId      uint
-		PlatformName    string
-		ReviewId        uint
-		AssessmentId    uint
-		ManifestId      uint
-		QuestionnaireId uint
-		AnalysisId      uint
-		Effort          int
+		IdentityId         uint
+		IdentityName       string
+		ServiceId          uint
+		ServiceName        string
+		OwnerId            uint
+		OwnerName          string
+		ContributorId      uint
+		ContributorName    string
+		WaveId             uint
+		WaveName           string
+		PlatformId         uint
+		PlatformName       string
+		ReviewId           uint
+		AssessmentId       uint
+		AssessmentSections []byte
+		ManifestId         uint
+		QuestionnaireId    uint
+		AnalysisId         uint
+		Effort             int
 	}
 	db := h.DB(ctx)
 	db = db.Select(
@@ -224,6 +225,7 @@ func (h ApplicationHandler) List(ctx *gin.Context) {
 		"pf.Name            PlatformName",
 		"rv.ID              ReviewId",
 		"at.ID              AssessmentId",
+		"at.Sections        AssessmentSections",
 		"mf.ID              ManifestId",
 		"at.QuestionnaireID QuestionnaireId",
 		"an.ID              AnalysisId",
@@ -298,6 +300,7 @@ func (h ApplicationHandler) List(ctx *gin.Context) {
 				ref := model.Assessment{}
 				ref.ID = m.AssessmentId
 				ref.QuestionnaireID = m.QuestionnaireId
+				_ = json.Unmarshal(m.AssessmentSections, &ref.Sections)
 				assessments[m.AssessmentId] = ref
 			}
 			if m.ManifestId > 0 {