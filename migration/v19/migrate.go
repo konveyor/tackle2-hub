1ea0a38d609001a0b3074779f7e401314a7d9744
Jeff Ortel <jortel@redhat.com>
2025-09-10 11:01:02 -0500

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- migration/v19/migrate.go
+++ migration/v19/migrate.go
@@ -1,20 +1,60 @@
 package v19
 
 import (
-	"github.com/jortel/go-utils/logr"
+	liberr "github.com/jortel/go-utils/error"
 	"github.com/konveyor/tackle2-hub/migration/v19/model"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
-
-var log = logr.WithName("migration|v18")
 
 type Migration struct{}
 
 func (r Migration) Apply(db *gorm.DB) (err error) {
+	err = r.migrateIdentities(db)
+	if err != nil {
+		return
+	}
 	err = db.AutoMigrate(r.Models()...)
 	return
 }
 
 func (r Migration) Models() []any {
 	return model.All()
+}
+
+func (r Migration) migrateIdentities(db *gorm.DB) (err error) {
+	migrator := db.Migrator()
+	err = migrator.RenameTable("ApplicationIdentity", "saved")
+	if err != nil {
+		err = liberr.Wrap(err)
+		return
+	}
+	err = db.AutoMigrate(&model.ApplicationIdentity{})
+	if err != nil {
+		err = liberr.Wrap(err)
+		return
+	}
+	type M struct {
+		model.ApplicationIdentity
+		Kind string
+	}
+	var saved []M
+	db2 := db.Table("saved")
+	db2 = db2.Joins("INNER JOIN Identity id ON  id.ID = saved.ApplicationIdentity.IdentityID")
+	err = db2.Table("appIdSaved").Find(&saved).Error
+	if err != nil {
+		err = liberr.Wrap(err)
+		return
+	}
+	db3 := db.Clauses(clause.OnConflict{DoNothing: true})
+	for _, m := range saved {
+		m.Role = m.Kind
+		err = db3.Create(&m).Error
+		if err != nil {
+			err = liberr.Wrap(err)
+			return
+		}
+	}
+	err = migrator.DropTable("saved")
+	return
 }
