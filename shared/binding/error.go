2e3050b325efae033a02426729e959f2ac488e72
Jeff Ortel <jortel@redhat.com>
2026-01-19 18:39:24 -0600

    :bug: Errors must be aliased.
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- shared/binding/error.go
+++ shared/binding/error.go
@@ -1,86 +1,17 @@
 package binding
 
 import (
-	"errors"
-	"io"
-	"net/http"
-	"strconv"
-	"strings"
+	"github.com/konveyor/tackle2-hub/shared/binding/client"
 )
 
 // RestError reports REST errors.
-type RestError struct {
-	Reason string
-	Method string
-	Path   string
-	Status int
-	Body   string
-}
-
-func (e *RestError) Is(err error) (matched bool) {
-	_, matched = err.(*RestError)
-	return
-}
-
-func (e *RestError) Error() (s string) {
-	s = e.Reason
-	return
-}
-
-func (e *RestError) With(r *http.Response) {
-	e.Method = r.Request.Method
-	e.Path = r.Request.URL.Path
-	e.Status = r.StatusCode
-	if r.Body != nil {
-		body, err := io.ReadAll(r.Body)
-		if err == nil {
-			e.Body = string(body)
-		}
-	}
-	s := strings.ToUpper(e.Method)
-	s += " "
-	s += e.Path
-	s += " failed: "
-	s += strconv.Itoa(e.Status)
-	s += "("
-	s += http.StatusText(e.Status)
-	s += ")"
-	if e.Body != "" {
-		s += " body: "
-		s += e.Body
-	}
-	e.Reason = s
-}
+type RestError = client.RestError
 
 // Conflict reports 409 error.
-type Conflict struct {
-	RestError
-}
-
-func (e *Conflict) Is(err error) (matched bool) {
-	var inst *Conflict
-	matched = errors.As(err, &inst)
-	return
-}
+type Conflict = client.Conflict
 
 // NotFound reports 404 error.
-type NotFound struct {
-	RestError
-}
-
-func (e *NotFound) Is(err error) (matched bool) {
-	var inst *NotFound
-	matched = errors.As(err, &inst)
-	return
-}
+type NotFound = client.NotFound
 
 // EmptyBody reports an empty body.
-type EmptyBody struct {
-	RestError
-}
-
-func (e *EmptyBody) Is(err error) (matched bool) {
-	var inst *EmptyBody
-	matched = errors.As(err, &inst)
-	return
-}
+type EmptyBody = client.EmptyBody
