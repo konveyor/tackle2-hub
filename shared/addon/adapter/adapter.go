da116f61bd7a638345ff8e62fabce591c8c920fd
Jeff Ortel <jortel@redhat.com>
2026-01-20 16:33:15 -0600

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- shared/addon/adapter/adapter.go
+++ shared/addon/adapter/adapter.go
@@ -155,14 +155,14 @@ func (h *Adapter) Run(addon func() error) {
 
 // Client returns a configured rich-client.
 func (h *Adapter) Client() (richClient *RichClient) {
-	richClient = binding.New(Settings.Hub.Token)
+	richClient = binding.New(Settings.Hub.URL)
 	richClient.Client.Use(api.Login{Token: Settings.Hub.Token})
 	return
 }
 
 // New builds a new Addon Adapter object.
 func New() (adapter *Adapter) {
-	richClient := binding.New(Settings.Hub.Token)
+	richClient := binding.New(Settings.Hub.URL)
 	richClient.Client.Use(api.Login{Token: Settings.Hub.Token})
 	adapter = &Adapter{
 		Task: Task{
