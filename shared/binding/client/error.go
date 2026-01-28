7a60a5dca92d53777c67555bd512dc695b510229
Jeff Ortel <jortel@redhat.com>
2026-01-28 07:30:46 -0600

    :bug: Fix internal vs shared error aliases. The internal/api errors are used to report errors within the web stack. The shared/api errors are used to catch errors when using the binding.
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- shared/binding/client/error.go
+++ shared/binding/client/error.go
@@ -1,87 +1,23 @@
 package client
 
 import (
-	"errors"
-	"io"
-	"net/http"
-	"strconv"
-	"strings"
+	"github.com/konveyor/tackle2-hub/shared/api"
 )
 
 // RestError reports REST errors.
-type RestError struct {
-	Reason string
-	Method string
-	Path   string
-	Status int
-	Body   string
-}
+type RestError = api.RestError
 
-func (e *RestError) Is(err error) (matched bool) {
-	var inst *RestError
-	matched = errors.As(err, &inst)
-	return
-}
+// BadRequestError reports bad request errors.
+type BadRequestError = api.BadRequestError
 
-func (e *RestError) Error() (s string) {
-	s = e.Reason
-	return
-}
-
-func (e *RestError) With(r *http.Response) {
-	e.Method = r.Request.Method
-	e.Path = r.Request.URL.Path
-	e.Status = r.StatusCode
-	if r.Body != nil {
-		body, err := io.ReadAll(r.Body)
-		if err == nil {
-			e.Body = string(body)
-		}
-	}
-	s := strings.ToUpper(e.Method)
-	s += " "
-	s += e.Path
-	s += " failed: "
-	s += strconv.Itoa(e.Status)
-	s += "("
-	s += http.StatusText(e.Status)
-	s += ")"
-	if e.Body != "" {
-		s += " body: "
-		s += e.Body
-	}
-	e.Reason = s
-}
+// Forbidden reports auth errors.
+type Forbidden = api.Forbidden
 
 // Conflict reports 409 error.
-type Conflict struct {
-	RestError
-}
-
-func (e *Conflict) Is(err error) (matched bool) {
-	var inst *Conflict
-	matched = errors.As(err, &inst)
-	return
-}
+type Conflict = api.Conflict
 
 // NotFound reports 404 error.
-type NotFound struct {
-	RestError
-}
-
-func (e *NotFound) Is(err error) (matched bool) {
-	var inst *NotFound
-	matched = errors.As(err, &inst)
-	return
-}
+type NotFound = api.NotFound
 
 // EmptyBody reports an empty body.
-type EmptyBody struct {
-	RestError
-}
-
-func (e *EmptyBody) Is(err error) (matched bool) {
-	var inst *EmptyBody
-	matched = errors.As(err, &inst)
-	return
-}
+type EmptyBody = api.EmptyBody
