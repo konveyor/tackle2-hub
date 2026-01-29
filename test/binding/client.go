0ea65cec0cd4be815778c2b28e0b8170c1d53d0e
Jeff Ortel <jortel@redhat.com>
2026-01-29 07:51:57 -0600

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

---
+++ test/binding/client.go
@@ -0,0 +1,32 @@
+package binding
+
+import (
+	"os"
+
+	"github.com/konveyor/tackle2-hub/shared/binding"
+	"github.com/konveyor/tackle2-hub/shared/settings"
+)
+
+const (
+	User     = "USER"
+	Password = "PASSWORD"
+)
+
+var (
+	Settings = &settings.Settings.Addon.Hub
+	client   *binding.RichClient
+)
+
+func init() {
+	client = binding.New(Settings.URL)
+	client.Client.SetRetry(uint8(1))
+	user := os.Getenv(User)
+	password := os.Getenv(Password)
+	if user == "" || password == "" {
+		return
+	}
+	err := client.Login(user, password)
+	if err == nil {
+		panic(err)
+	}
+}
